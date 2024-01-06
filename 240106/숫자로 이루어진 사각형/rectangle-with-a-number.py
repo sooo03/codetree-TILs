85754892be0564ddfee8fef86bfe2d4cc922d2da
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-01-06 23:56:26 +0900

    Title: 숫자로 이루어진 사각형, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240106/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,11 @@
+def rect(n):
+    cnt = 1
+    for _ in range(n):
+        for _ in range(n):
+            print(cnt, end=" ")
+            cnt += 1
+            if cnt == 10:
+                cnt = 1
+        print()
+n = int(input())
+rect(n)
