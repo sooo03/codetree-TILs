e1471607881b92d82f9f5e364067ff3a720a7c83
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-01-12 16:44:13 +0900

    Title: 좀 더 어려운 수학 점수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240112/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,12 @@
+a_math, a_eng = map(int, input().split())
+b_math, b_eng = map(int, input().split())
+
+if a_math > b_math:
+    print("A")
+elif b>math > a_math:
+    print("B")
+elif a_math == b_math:
+    if a_eng > b_eng:
+        print("A")
+    else:
+        print("B")
