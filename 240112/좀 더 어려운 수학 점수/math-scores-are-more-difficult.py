0a09738b19929f2700342391aa82a1bf916bf5e7
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-01-12 16:45:22 +0900

    Title: 좀 더 어려운 수학 점수, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240112/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 240112/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -3,7 +3,7 @@ b_math, b_eng = map(int, input().split())
 
 if a_math > b_math:
     print("A")
-elif b>math > a_math:
+elif b_math > a_math:
     print("B")
 elif a_math == b_math:
     if a_eng > b_eng:
