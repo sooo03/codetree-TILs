ae30fe9a21b481f2272f83dd8419a8450966a5f7
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-01-13 23:19:02 +0900

    Title: 특정 숫자 도달하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240113/특정 숫자 도달하기/reaching-specific-number.py
+++ 240113/특정 숫자 도달하기/reaching-specific-number.py
@@ -1,16 +1,14 @@
 arr = list(map(int, input().split()))
 
-new_arr = list()
-sum_val, mean = 0, 0
+new_arr = 0
+cnt = 0
 
-for i in range(len(arr)):
-    if arr[i] >= 250:
+for i in arr:
+    if i >= 250:
         break
-    sum_val += arr[i]
+    sum_val += i 
+    cnt += 1
 
-if i != 0:
-    mean = sum_val / i
-else:
-    mean = 0
+avg = sum_val / cnt
 
 print(f"{sum_val} {mean:.1f}")
