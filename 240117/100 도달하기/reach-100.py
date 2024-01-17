e866c9e190a4fd2a835f7aa0793b63fd0ff035b9
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-01-17 14:56:25 +0900

    Title: 100 도달하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240117/100 도달하기/reach-100.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = []
+arr[0], arr[1] = 1, n
+sum_val = 0
+
+for i in range():
+    if sum_val > 100:
+        break
+    arr.append(arr[-1]+arr[-2])
+    sum_val += arr[i]
+print(sum_val)
