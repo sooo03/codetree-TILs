b0d3dd88c3ebd7eed5a5fec3d972d1a43816ab75
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-01-23 13:31:19 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240123/배열 놀이/play-with-array.py
@@ -0,0 +1,23 @@
+n, q = map(int, input().split())
+element = list(map(int, input().split()))
+first_ques, a1 = map(int, input().split())
+second_ques, a2 = map(int, input().split())
+third_ques, a3, b3 = map(int, input().split())
+
+#1 a
+print(element[a1-1])
+
+#2 a
+cnt = 0
+for i in range(len(element)):
+    if a2 != element[i]: #숫자 a가 없는 경우
+        cnt = 0
+    #숫자 a가 있는 경우
+    else:
+        cnt = i + 1
+        break
+print(cnt)
+
+#3 a b
+for i in range(a3-1, b3):
+    print(element[i], end=' ')
