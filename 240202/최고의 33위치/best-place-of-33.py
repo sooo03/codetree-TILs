78b5b7899ae2d9ccf2c4c66d54a1510698719bed
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-02 22:39:34 +0900

    Title: 최고의 33위치, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240202/최고의 33위치/best-place-of-33.py
+++ 240202/최고의 33위치/best-place-of-33.py
@@ -1,14 +1,13 @@
-# 재귀함수 기반의 백트래킹
-n = int(input())
-arr = [list(map(int,input().split())) for _ in range(n) ]
-max_sum=0
+n=int(input())
+array=[list(map(int,input().split())) for _ in range(n)]
+result=0
 
-for x in range(0, n-2):
-    for y in range(0, n-2):
-        key = 0
+for x in range(0,n-2):
+    for y in range(0,n-2):
+        key=0
         for i in range(3):
             for j in range(3):
-                if array[x+i][y+j] == 1:
-                    key += 1    
-        max_sum= max(max_sum, key)
-print(max_sum)
+                if array[x+i][y+j]==1:
+                    key+=1    
+        result=max(result,key)
+print(result)
