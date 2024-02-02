ff35ef1b1482a3c7d7cb6adc7bbdf6aef7372c02
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-03 08:33:42 +0900

    Title: 피보나치 수, Time: 5047ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240203/피보나치 수/fibonacci-number.py
+++ 240203/피보나치 수/fibonacci-number.py
@@ -1,8 +1,10 @@
 n = int(input())
-dp[1] = 1
-dp[2] = 1
 
-for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+def fibo(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    return fibo(n-1) + fibo(n-2)
 
-print(dp[n])
+print(fibo(n))
