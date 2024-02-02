1afe46c325900895ab38693c402bf5f30b9f59dc
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-02 22:40:08 +0900

    Title: 최고의 33위치, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240202/최고의 33위치/best-place-of-33.py
+++ 240202/최고의 33위치/best-place-of-33.py
@@ -1,13 +1,13 @@
-n=int(input())
-array=[list(map(int,input().split())) for _ in range(n)]
-result=0
+n = int(input())
+array = [list(map(int,input().split())) for _ in range(n)]
+result = 0
 
-for x in range(0,n-2):
-    for y in range(0,n-2):
-        key=0
+for x in range(0, n-2):
+    for y in range(0, n-2):
+        key = 0
         for i in range(3):
             for j in range(3):
-                if array[x+i][y+j]==1:
-                    key+=1    
-        result=max(result,key)
+                if array[x+i][y+j] == 1:
+                    key += 1    
+        result = max(result, key)
 print(result)
