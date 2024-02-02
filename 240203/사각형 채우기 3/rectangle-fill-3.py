f11ff4b0b6b41ff8fe7b0a40e4a4b46b5b73fd89
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-03 08:55:40 +0900

    Title: 사각형 채우기 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240203/사각형 채우기 3/rectangle-fill-3.py
+++ 240203/사각형 채우기 3/rectangle-fill-3.py
@@ -1,10 +1,14 @@
 MOD = 1000000007
 
 n = int(input())
-dp = [0] * 1001
-dp[1], dp[2] = 2, 7
+A = [0] * 1001
+A[1], A[2] = 2, 7
+
+#마지막 1개가 한 칸만 채워진 경우
+B[1], B[2] = 1, 3
 
 for i in range(3, n+1):
-    dp[i] = (dp[i-1] + dp[i-2]) % MOD
+    A[i] = (A[i-1]*2 + A[i-2] + B[i-1]*2) % MOD
+    B[i] = (A[i-1] + B[i-2] % MOD)
 
-print(dp[n])
+print(A[n])
