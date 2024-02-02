2cc1553f4d1c2b9394ba565bf50052e488141853
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-03 08:47:42 +0900

    Title: 계단 오르기, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/계단 오르기/climbing-stairs.py
+++ 240203/계단 오르기/climbing-stairs.py
@@ -5,6 +5,7 @@ n = int(input())
 dp = [0] * 1001
 dp[0], dp[1], dp[2], dp[3] = 1, 0, 1, 1
 
-for i in range(4, n+1):
-    dp[i] = (dp[n-2] + dp[n-3]) % 10007
+for i in range(4, n + 1):
+    dp[i] = (dp[i - 2] + dp[i - 3]) % 10007
+
 print(dp[n])
