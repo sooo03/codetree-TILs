a906ad88cf8db4a05591dea3b465b34586437e9b
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-03 09:05:15 +0900

    Title: 서로 다른 BST 개수 세기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ 240203/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -2,14 +2,17 @@ n = int(input())
 dp = [0 for _ in range(20)]
 
 def count_bst(n):
-    num_of_bst = 0
+    bst_num = 0
+    
     for i in range(n):
-        num_of_bst += dp[i] * dp[n-i-1]
-    return num_of_bst
+        bst_num += dp[i] * dp[n - i - 1]
+        
+    return bst_num
 
-dp[0], dp[1] = 1, 1
 
-for i in range(2, n+1):
-    dp[i] = num_of_bst(i)
+dp[0] = dp[1] = 1
 
+for i in range(2, n + 1):
+    dp[i] = count_bst(i)
+    
 print(dp[n])
