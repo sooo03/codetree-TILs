e2e9ce646c791ce9c82213901c6ade9e2965260b
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-03 09:03:46 +0900

    Title: 서로 다른 BST 개수 세기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240203/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ 240203/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -1,8 +1,15 @@
 n = int(input())
 dp = [0] * 20
 
-for i in range(1, n+1):
-    for j in range(0, j):
-        dp[i] += dp[j] * dp[i-j-1]
+def count_bst(n):
+    num_of_bst = 0
+    for i in range(n):
+        num_of_bst += dp[i] * dp[n-i-1]
+    return num_of_bst
+
+dp[0], dp[1] = 1, 1
+
+for i in range(2, n+1):
+    dp[i] = num_of_bst(i)
 
 print(dp[n])
