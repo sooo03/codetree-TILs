ba50ee4cf74a71624e3eccc94c4f27270dacbfee
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-04 18:18:29 +0900

    Title: 크기가 n인 순열, Time: 254ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240204/크기가 n인 순열/n-permutation.py
@@ -0,0 +1,23 @@
+n = int(input())
+visited = [False] * (n + 1)
+picked = []
+
+
+def get_permutation(cnt):
+    if cnt == n:
+        for elem in picked:
+            print(elem, end=" ")
+        print()
+    for i in range(1, n + 1):
+        if visited[i]: 
+            continue
+        
+        visited[i] = True
+        picked.append(i)
+
+        get_permutation(cnt + 1)
+
+        visited[i] = False
+        picked.pop()
+
+get_permutation(0)
