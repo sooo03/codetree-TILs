1cd339dad6cd4725f44e23238cf1db00fc7e8a5d
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-04 18:03:59 +0900

    Title: 행복한 수열의 개수, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240204/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240204/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,43 +1,43 @@
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-# print(graph)
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+seq = [0 for _ in range(n)]
 
-line = [0] * n
 
-def happy_sequence():
-    same_cnt = 1
-
+def is_happy_sequence():
+    # 주어진 seq가 행복한 수열인지 판단하는 함수
+    consecutive_count, max_ccnt = 1, 1
     for i in range(1, n):
-        if same_cnt == m:
-            return True
-        if line[i-1] == line[i]:
-            same_cnt += 1 
+        if seq[i - 1] == seq[i]:
+            consecutive_count += 1
         else:
-            same_cnt = 1
-    return False
-
-happy_cnt = 0
-
-# 가로로 행복한 수열 
-for r in range(n):
-    line = graph[r][:]
-    # print("가로")
-    # print(line)
-
-    if happy_sequence():
-        print(r) #가로
-        happy_cnt += 1
-
-# 세로로 행복한 수열
-line = [0] * n
-for c in range(n):
-    for r in range(n):
-        line[r] = graph[r][c]     # 세로로 숫자들을 모아 새로운 수열 만들기
-    # print("세로")
-    # print(line)
+            consecutive_count = 1
+        
+        max_ccnt = max(max_ccnt, consecutive_count)
     
-    if happy_sequence():
-        print(c) #세로
-        happy_cnt += 1
+    # 최대로 연속한 회수가 m이상이면 true를 반환함
+    return max_ccnt >= m
 
-print(happy_cnt)
+
+num_happy = 0
+
+# 먼저 가로로 행복한 수열의 수를 셈
+for i in range(n):
+    seq = grid[i][:]
+
+    if is_happy_sequence():
+        num_happy += 1
+
+# 세로로 행복한 수열의 수를 셈
+for j in range(n):
+    # 세로로 숫자들을 모아 새로운 수열을 만듦
+    for i in range(n):
+        seq[i] = grid[i][j]
+
+    if is_happy_sequence():
+        num_happy += 1
+
+print(num_happy)
