ccbc31f2e3faeda714010dfa531704182de9f646
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-09 22:25:00 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240209/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,16 @@
+n = int(input())
+a = list(map(int, input().split()))
+prev_max_idx = n
+
+while True:
+    max_idx = 0
+
+    for i in range(1, prev_max_idx):
+        if a[i] > a[max_idx]:
+            max_idx = i
+    print(max_idx + 1, end=" ")
+
+    if max_idx == 0:
+        break
+        
+    prev_max_idx = max_idx
